ccfe7b672b45e8e880401ae2b1a5e5db8db46161
Ivan Satsiuk <sativ01.ca@gmail.com>
2020-12-06 22:23:50 +0100

    add test route

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
   res.send({ hello: 'world'});
 })
 
+app.get('/test', (req, res) => {
+  res.send({ test: 'hello'});
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
