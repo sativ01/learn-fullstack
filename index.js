0ec85367ea13b946940a8265a2cc9b3ebb6e5668
Ivan Satsiuk <sativ01.ca@gmail.com>
2020-12-14 23:44:22 +0100

    add cookies session

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session');
+const passport = require('passport')
+
 require('./models/User')
 require('./services/passport')
 const mongoKeys = require('./secret/authKey').mongo;
+const cookieKey = require('./secret/authKey').cookieSession;
 
 mongoose.connect(mongoKeys.uri)
 
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey.encriptionKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const mongoose = require('mongoose');
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../secret/authKey.js');
@@ -8,14 +9,39 @@ const { google } = keys;
 const modelNames = require('../models/modelNames');
 const UsersModel = mongoose.model(modelNames.users);
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
+passport.deserializeUser(async (id, done) => {
+  const user = await UsersModel.findById(id);
+  done(null, user)
+})
+
 passport.use(new GoogleStrategy({
   clientID: google.client.ID,
   clientSecret: google.client.SECRET,
   callbackURL: '/auth/google/callback'
 },
-(accessToken, refreshToken, profile, done)  => {
-  new UsersModel({
-    googleId: profile.id,
-    name: profile.displayName,
-  }).save();
+async (accessToken, refreshToken, profile, done)  => {
+  let error = null;
+  let user = null;
+  try{
+    user = await UsersModel.findOne({googleId: profile.id});
+
+    if(!user){
+      user = await new UsersModel({
+        googleId: profile.id,
+        name: profile.displayName,
+      }).save();
+    }
+    done(null, user);
+    console.log(`done ${user.name}`);
+  } catch(err){
+    error = err;
+    done(error, user);
+    console.log(`err ${user.name}`);
+  }
+
 }));
+
